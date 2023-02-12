2d79abe7f7ee8f70bc6647128742f6dfd8a212e2
robrob le codeur <robin.auxietre@epitech.eu>
2023-02-12 23:30:04 +0100

    add win

--- Chauve-Souris/Assets/GIrl/Scripts/CheckWin.cs
+++ Chauve-Souris/Assets/GIrl/Scripts/CheckWin.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class CheckWin : MonoBehaviour
 {
+    private GameManager manager;
+
+    private void Start () {
+        manager = GameManager.instance;
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
+            manager.ChangeScene("win");
             Debug.Log("You Win!");
         }
     }
